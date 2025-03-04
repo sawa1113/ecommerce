02a394db0ad7ff45ea789799c2364858c927f1e4
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2025-03-04 13:03:36 +0900

    productsコントローラーの作成

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,47 @@
+class Admin::ProductsController < ApplicationController
+  # 商品を閲覧・作成・編集する機能はログイン済の管理者のみ権限を付与するのでauthenticate_admin!メソッドを記載。
+  before_action :authenticate_admin!
+  # 共通するアクションはメソッドでまとめて記載。
+  before_action :set_product, only: %i[show edit update]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :stock)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
     sessions: 'admin/sessions'
   }
   devise_for :customers, controllers: {
-    sessions: 'customer/sessions',
+    sessions: 'customer/sessions'
     registrations: 'customer/registrations'
   }
   root to: 'pages#home'
+  namespace :admin do
+    resources :products, only: %i[index show new create edit update]
+  end
 
   get '/up/', to: 'up#index', as: :up
   get '/up/databases', to: 'up#databases', as: :up_databases
