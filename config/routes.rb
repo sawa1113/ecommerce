3f115e73a416ba1a1ab7064a1ede3070e5d2b02c
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2025-03-04 14:24:19 +0900

    viewsの作成(顧客用)

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products, only: %i[index show new create edit update]
   end
-  scpoe module: :customer do
+  scope module: :customer do
     resources :products, only: %i[index show]
   end
 
